de0300fee4338deac95de60796c70308cf255433
StopScreen <149250971+StopScreen@users.noreply.github.com>
2024-02-17 17:24:46 +0300

    Add files via upload

---
+++ Средний шаг.py
@@ -0,0 +1,11 @@
+print("(hZ) StopScreen Studios (SSS). All right reversed.")
+print("Добро Пожаловать в программу для вычисления среднего шага!!!")
+print()
+print()
+St = float(input("Введите кол-во сделанных шагов: "))
+S = float(input("Введите проделанное расстояние (В метрах): "))
+print("Загрузка результата...")
+import time
+Sr = S / St
+print("Средний шаг равен", Sr)
+time.sleep(10)
